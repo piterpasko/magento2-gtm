198db9563198f79d0af15a67c04d7ba7420f77a7
petropasko <p.pasko@mobilunity.com>
2018-10-04 13:33:01 +0300

    initial commit

---
+++ registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by http://madepeople.se
+ * User: Piter Pasko
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Madepeople_GTM',
+    __DIR__
+);
